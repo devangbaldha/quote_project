6bfc4528ba745124467c793a210e4ca857b881f8
NightShift <nightshift@Trilokn-Mac-Mini-09s-Mac-mini.local>
2026-08-11 23:23:30 +0530

    update

--- my_cron/settings.py
+++ my_cron/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = True
 # os.environ.get("DEBUG", "False") == "True"
 ALLOWED_HOSTS = os.environ.get(
-    "quote-project.onrender.com",
+    "ALLOWED_HOSTS",
     "localhost,127.0.0.1"
 ).split(",")
 
